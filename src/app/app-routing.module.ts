cd9b7c9100ce272166b13d2ad9d18182721075f3
Anevo <cezar.vulcu@gmail.com>
2019-04-01 11:09:01 +0100

    Updated imports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MainPanelComponent } from "./main-panel/main-panel.component";
 
 @NgModule({
   imports: [
